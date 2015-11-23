8d0c1b4966bc837be4e0d90c8ef8afd4de5ac813
Anuthida Lertnamwongwan <rasnaaa.l@gmail.com>
2015-11-23 10:14:35 +0700

    23/11/15 10:15am

--- Week2/js/add-content.js
+++ Week2/js/add-content.js
@@ -1,1 +1,11 @@
-document.write("Hello Week2!");
+var price;
+var quantity;
+var total;
+
+price=5;
+quantity = 14;
+total = price * quantity;
+
+//document.write("Total is " + total);
+var totalPriceEle= document.getElementById("totalPrice");
+totalPriceEle.textContent = "$"+ total;
